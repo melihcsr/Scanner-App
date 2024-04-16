ab4ad78ba685f22b1e0aca4ba39e1cfd8408525c
Melih Cesur <mlhcsr@yahoo.com>
2024-04-16 19:01:51 +0300

    Initial Commit

---
+++ Scanner App/Scanner_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scanner_AppApp.swift
+//  Scanner App
+//
+//  Created by Melih Cesur on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Scanner_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
